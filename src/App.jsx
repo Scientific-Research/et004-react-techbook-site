f04160b2e25e9ff2df769f5c28922cbc3d230fc0
Maximilian <scientificresearch309@gmail.com>
2022-11-14 17:05:11 +0100

    shows number of books

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import techbooks from "./data/techbooks.json";
 
+console.log(techbooks);
 function App() {
   return (
     <div className="App">
-      <h1>Tech Books</h1>
+      <h1>There are {techbooks.length} Tech Books</h1>
       <p>Test</p>
     </div>
   );
