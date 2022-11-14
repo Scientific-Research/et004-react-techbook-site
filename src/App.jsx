1b4d11bc342918bca50faf6e2fed13442c4c31d3
Maximilian <scientificresearch309@gmail.com>
2022-11-14 17:27:28 +0100

    showing all title

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <div className="App">
       <h1>There are {techbooks.length} Tech Books</h1>
-      <p>Test</p>
+      <div className="book">
+        {/* <p>{techbooks.map((item) => item.title)}</p> */}
+        {/* <ul>
+          {techbooks.map((item) => (
+            <li>{item.title}</li>
+          ))}
+        </ul> */}
+
+        {techbooks.map((item) => {
+          return <div>{item.title}</div>;
+        })}
+      </div>
     </div>
   );
 }
